6123efa225004d24741e74cfac832d2ea1128490
Global Name <sreeved@inciem.com>
2025-08-11 12:20:29 +0530

    Contact Page start

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const menuToggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu');
 
+// Toggle mobile menu
 menuToggle.addEventListener('click', () => {
     menu.classList.toggle('show');
 });
 
-// Optional: Close menu when clicking a link
+// Close menu when clicking a link
 document.querySelectorAll('#menu a').forEach(link => {
     link.addEventListener('click', () => {
         menu.classList.remove('show');
     });
+});
+
+// Change navbar background on scroll
+window.addEventListener("scroll", () => {
+    const nav = document.querySelector("nav");
+    if (window.scrollY > 10) { // when user scrolls down
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
 });
